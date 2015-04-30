c7ccb5470c1d0a9283e4f339f5dc031eca300f05
kamatama41 <shiketaudonko41@gmail.com>
2015-04-30 23:23:36 +0900

    chapter2

---
+++ mindmap/src/main/java/com/kamatama41/vertx/MindMap.java
@@ -0,0 +1,27 @@
+package com.kamatama41.vertx;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class MindMap {
+    private Integer id;
+    private String name;
+
+    @JsonCreator
+    public MindMap(
+            @JsonProperty(value = "_id", required = false) Integer id,
+            @JsonProperty("name") String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @JsonProperty("_id")
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
