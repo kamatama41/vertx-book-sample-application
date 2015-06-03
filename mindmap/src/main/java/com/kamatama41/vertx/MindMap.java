310581595a9cf8073a3af9596858dddad545deaa
kamatama41 <shiketaudonko41@gmail.com>
2015-06-03 23:46:29 +0900

    [chapter4] Implement EditorVerticle methods.

--- mindmap/src/main/java/com/kamatama41/vertx/MindMap.java
+++ mindmap/src/main/java/com/kamatama41/vertx/MindMap.java
@@ -5,7 +5,10 @@ import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.core.json.impl.Json;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 public class MindMap {
 
@@ -13,10 +16,6 @@ public class MindMap {
 
     public MindMap(JsonObject source) {
         this.source = source;
-    }
-
-    public MindMap() {
-        this(new JsonObject().putArray("children", new JsonArray()));
     }
 
     public String getId() {
@@ -27,12 +26,12 @@ public class MindMap {
         source.putString("_id", id);
     }
 
-    public String getName() {
-        return source.getString("name");
+    public Node getNode() {
+        return new Node(source);
     }
 
-    public void setName(String name) {
-        source.putString("name", name);
+    public JsonObject toJson() {
+        return source;
     }
 
     @Override
@@ -47,24 +46,75 @@ public class MindMap {
             this.source = source;
         }
 
-        private Node() {
-            this(new JsonObject().putArray("children", new JsonArray()));
+        public Node() {
+            this(new JsonObject());
+            setKey(newNodeKey());
         }
 
         public String getKey() {
             return source.getString("key");
         }
 
-        public List<Node> getChildren() {
-            List<Node> result = new ArrayList<>();
-            for (Object child : source.getArray("children")) {
-                if(!(child instanceof JsonObject)) {
-                    throw new IllegalStateException("Node.child isn't unexpected tyoe = " + child.getClass().getCanonicalName());
-                }
-                result.add(new Node((JsonObject)child));
-            }
-            return result;
+        public void setKey(String key) {
+            this.source.putString("key", key);
         }
+
+        public String getName() {
+            return source.getString("name");
+        }
+
+        public void setName(String name) {
+            source.putString("name", name);
+        }
+
+        public void addChild(Node child) {
+            JsonArray children = source.getArray("children");
+            if(children == null) {
+                children = new JsonArray();
+                source.putArray("children", children);
+            }
+            children.add(child.source);
+        }
+
+        public List<Node> removeChild(String key) {
+            JsonArray children = source.getArray("children");
+            if(children == null) {
+                return Collections.emptyList();
+            }
+            final Iterator<Object> iterator = children.iterator();
+            List<Node> removed = new ArrayList<>();
+            while (iterator.hasNext()) {
+                final JsonObject child = (JsonObject) iterator.next();
+                if(child.getString("name").equals(key)) {
+                    iterator.remove();
+                    removed.add(new Node(child));
+                }
+            }
+            return removed;
+        }
+
+        public List<Node> getChildren() {
+            return createChildren(source);
+        }
+
+        public JsonObject toJson() {
+            return source;
+        }
+    }
+
+    private static List<Node> createChildren(JsonObject source) {
+        List<Node> result = new ArrayList<>();
+        for (Object child : source.getArray("children")) {
+            if(!(child instanceof JsonObject)) {
+                throw new IllegalStateException("Node.child isn't unexpected tyoe = " + child.getClass().getCanonicalName());
+            }
+            result.add(new Node((JsonObject)child));
+        }
+        return result;
+    }
+
+    private static String newNodeKey() {
+        return UUID.randomUUID().toString();
     }
 
 }
---
+++ mindmap/src/main/java/com/kamatama41/vertx/MindMapEditorVerticle.java
@@ -0,0 +1,136 @@
+package com.kamatama41.vertx;
+
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.platform.Verticle;
+
+import java.util.List;
+
+/**
+ * @author SHINICHI Ishimura
+ */
+public class MindMapEditorVerticle extends Verticle {
+
+    @Override
+    public void start() {
+        vertx.eventBus().registerHandler("mindMaps.editor.addNode", (Message<JsonObject> request) -> {
+            final JsonObject reqBody = request.body();
+            final String mindMapId = reqBody.getString("mindMapId");
+            final String parentKey = reqBody.getString("parentKey");
+            final String name = reqBody.getString("name");
+
+            vertx.eventBus().send("mindMaps.find"
+                    , new JsonObject().putString("_id", mindMapId)
+                    , (Message<JsonObject> response) -> {
+                        final JsonObject mindMapJson = response.body().getObject("mindMap");
+                        if(mindMapJson == null) {
+                            return;
+                        }
+
+                        MindMap mindMap = new MindMap(mindMapJson);
+                        MindMap.Node parent = findNodeByKey(mindMap.getNode(), parentKey);
+                        MindMap.Node newNode = new MindMap.Node();
+
+                        if(name != null) {
+                            newNode.setName(name);
+                        } else {
+                            newNode.setName("Click to edit");
+                        }
+                        parent.addChild(newNode);
+
+                        vertx.eventBus().send("mindMaps.save", mindMap.toJson(), (Message<JsonObject> ignore) ->{
+                            publishMindMapEvent(mindMap
+                                    , new JsonObject()
+                                    .putString("event", "nodeAdded")
+                                            .putString("parentKey", parentKey)
+                                            .putObject("node", newNode.toJson())
+                            );
+                        });
+                    }
+            );
+        });
+
+        vertx.eventBus().registerHandler("mindMaps.editor.renameNode", (Message<JsonObject> request) -> {
+            final JsonObject reqBody = request.body();
+            final String mindMapId = reqBody.getString("mindMapId");
+            final String key = reqBody.getString("key");
+            final String newName = reqBody.getString("newName");
+            vertx.eventBus().send("mindMaps.find"
+                    , new JsonObject().putString("_id", mindMapId)
+                    , (Message<JsonObject> response) -> {
+                        final JsonObject mindMapJson = response.body().getObject("mindMap");
+                        if(mindMapJson == null) {
+                            return;
+                        }
+
+                        MindMap mindMap = new MindMap(mindMapJson);
+                        MindMap.Node node = findNodeByKey(mindMap.getNode(), key);
+                        if(node == null) {
+                            return;
+                        }
+                        node.setName(newName);
+                        vertx.eventBus().send("mindMaps.save", mindMap.toJson(), (Message<JsonObject> ignore) -> {
+                            publishMindMapEvent(mindMap
+                                    , new JsonObject()
+                                    .putString("event", "nodeRenames")
+                                    .putString("key", key)
+                                    .putString("newName", newName)
+
+
+                            );
+                        });
+                    }
+            );
+        });
+
+        vertx.eventBus().registerHandler("mindMaps.editor.deleteNode", (Message<JsonObject> request) -> {
+            final JsonObject reqBody = request.body();
+            final String mindMapId = reqBody.getString("mindMapId");
+            final String parentKey = reqBody.getString("parentKey");
+            final String key = reqBody.getString("key");
+            vertx.eventBus().send("mindMaps.find"
+                    , new JsonObject().putString("_id", mindMapId)
+                    , (Message<JsonObject> response) -> {
+                        final JsonObject mindMapJson = response.body().getObject("mindMap");
+                        if(mindMapJson == null) {
+                            return;
+                        }
+
+                        MindMap mindMap = new MindMap(mindMapJson);
+                        MindMap.Node parent = findNodeByKey(mindMap.getNode(), parentKey);
+                        final List<MindMap.Node> removed = parent.removeChild(key);
+
+                        vertx.eventBus().send("mindMaps.save", mindMap.toJson(), (Message<JsonObject> ignore) -> {
+                            removed.forEach(node -> {
+                                publishMindMapEvent(mindMap
+                                        , new JsonObject()
+                                        .putString("event", "nodeDeleted")
+                                        .putString("parentKey", parentKey)
+                                        .putString("key", key)
+                                );
+                            });
+                        });
+                    }
+            );
+        });
+    }
+
+    private MindMap.Node findNodeByKey(MindMap.Node root, String key) {
+        if(key.equals(root.getKey())) {
+            return root;
+        } else if(root.getChildren() != null) {
+            for (MindMap.Node child : root.getChildren()) {
+                MindMap.Node match = findNodeByKey(child, key);
+                if(match != null) {
+                    return match;
+                }
+            }
+        }
+        return null;
+    }
+
+    private void publishMindMapEvent(MindMap mindMap, Object event) {
+        vertx.eventBus().publish("mindMaps.event." + mindMap.getId(), event);
+    }
+
+}
