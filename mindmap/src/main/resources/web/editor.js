6fd3c5403be81ec5da56c3de7daf580c47920ec9
kamatama41 <shiketaudonko41@gmail.com>
2015-06-08 23:19:58 +0900

    [chapter4] WIP

--- mindmap/src/main/resources/web/editor.js
+++ mindmap/src/main/resources/web/editor.js
@@ -2,6 +2,8 @@ function MindMapEditor(mindMap, eventBus) {
     this.mindMap = mindMap;
     this.eventBus = eventBus;
     this.registerEventHandlers();
+    this.initVisualization();
+    this.renderVisualization();
 }
 
 MindMapEditor.width = 1280;
@@ -11,6 +13,47 @@ MindMapEditor.treeLayout = d3.layout.tree().size([MindMapEditor.height, MindMapE
 MindMapEditor.diagonalGenerator = d3.svg.diagonal().projection(function(d) {
     return [d.y, d.x];
 });
+
+MindMapEditor.prototype.renderVisualization = function() {
+    var self = this;
+    var nodes = MindMapEditor.treeLayout.nodes(this.mindMap).reverse();
+    nodes.forEach(function(d){
+        d.y = d.depth * MindMapEditor.levelWidth;
+    });
+    var node = this.vis.selectAll("g.node").data(nodes, function(d){
+        return d.key;
+    });
+    var nodeEnter = node.enter().append("svg:g")
+        .attr("class", "node").attr("opacity", "0").attr("transform", function(d){
+            return "translate(" + d.y + "," + d.x + ")";
+        });
+    nodeEnter.append("svg:circle").attr("r", 4.5).style("fill", "lightsteelblue").on("click", function(c){
+        self.addNode(c);
+    });
+    nodeEnter.append("svg:text").attr("x", 10).attr("by", ".35em").text(function(d){
+        return d.name;
+    }).on("click", function(d){
+        var text = prompt('Enter the name of this node', d.name);
+        if(text) {
+            self.renameNode(d, text);
+        }
+    });
+    node.transition().attr("opacity", "1").attr("transform", function(d) {
+        return "translate(" + d.y + "," + d.x + ")";
+    }).select("text").text(function(d){
+        return d.name;
+    });
+    node.exit().remove();
+    var link = this.vis.selectAll("path.link").data(MindMapEditor.treeLayout.links(nodes), function(d) {
+        return d.target.key;
+    });
+    link.enter().insert("svg:path", "g").attr("class", "link").attr("opacity", "0")
+        .attr("d", MindMapEditor.diagonalGenerator).on("click", function(l){
+            self.deleteNode(l.source, l.target);
+        });
+    link.transition().attr("d", MindMapEditor.diagonalGenerator).attr("opacity", "1");
+    link.exit().remove();
+};
 
 MindMapEditor.prototype.initVisualization = function() {
     this.vis = d3.select(".editor").html('').append("svg:svg")
@@ -26,7 +69,8 @@ MindMapEditor.prototype.registerEventHandlers = function() {
             case 'nodeRenamed': self.onNodeRenamed(event); break;
             case 'nodeDeleted': self.onNodeDeleted(event); break;
         }
-    })
+        self.renderVisualization();
+    });
 };
 
 MindMapEditor.prototype.onNodeAdded = function(event) {
